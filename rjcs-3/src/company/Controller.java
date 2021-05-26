1adfdf95bb170767eececdbcb0908ddc3ee8d406
何涔涌 <huaiyinhcy@163.com>
2021-05-26 10:42:27 +0800

    首位正负号

--- rjcs-3/src/company/Controller.java
+++ rjcs-3/src/company/Controller.java
@@ -4,10 +4,16 @@ public class Controller {
     public void textCon(String str) {
         Service service = new Service();
         int num1, num2;
-        for (int i = 0; i < str.length(); i++) {
+        char fm = ' ';
+        int ii = 0;
+        if (str.charAt(0) == '+' || str.charAt(0) == '-'){
+            fm = str.charAt(0);
+            ii = 1;
+        }
+        for (int i = ii; i < str.length(); i++) {
             if (str.charAt(i) == '+' || str.charAt(i) == '-' || str.charAt(i) == '*' || str.charAt(i) == '/' ) {
                 try {
-                    num1 = Integer.parseInt(str.substring(0, i).trim());
+                    num1 = Integer.parseInt((fm + (str.substring(ii, i).trim())).trim());
                     num2 = Integer.parseInt(str.substring(i + 1).trim());
                     if (str.charAt(i) == '+') System.out.println(service.plus(num1, num2));
                     if (str.charAt(i) == '-') System.out.println(service.minus(num1, num2));
